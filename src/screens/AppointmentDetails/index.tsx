2d41fc06b71b6d5f252044efc03279b46c7f6957
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2022-01-02 15:34:44 -0300

    Adicionando estrutura do banner na tela de detalhes

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,30 +1,44 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import Header from '../../components/Header'
+import { View, Text, ImageBackground } from 'react-native'
 
 //Apenas para teste
 import { RectButton } from 'react-native-gesture-handler'
-import Icon from '@expo/vector-icons/MaterialIcons' 
+import Icon from '@expo/vector-icons/MaterialIcons'
+
+import Header from '../../components/Header'
+import BannerImg from '../../assets/banner.png'
 
 import { styles } from './styles'
 import { theme } from '../../global/styles/theme'
 
 type Props = {
-  
+
 }
 
-export default function AppointmentDetails({}: Props){
+export default function AppointmentDetails({ }: Props) {
   return (
     <View style={styles.container}>
       <Header title='Titulo' action={
-        <RectButton style={{padding: 8,}}>
-          <Icon 
+        <RectButton style={{ padding: 8, }}>
+          <Icon
             name="share"
             size={24}
             color={theme.colors.primary}
           />
-        </RectButton>   
+        </RectButton>
       } />
+
+      <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+      >
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>League of Legends</Text>
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   )
 }
--- src/screens/AppointmentDetails/styles.ts
+++ src/screens/AppointmentDetails/styles.ts
@@ -1,3 +1,4 @@
+import { theme } from './../../global/styles/theme'
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
@@ -7,4 +8,26 @@ export const styles = StyleSheet.create({
   text: {
     color: '#000',
   },
+  banner: {
+    width: '100%',
+    height: 234,
+  },
+  bannerContent: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 24,
+    paddingBottom: 24,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+    marginBottom: 12,
+  },
+  subtitle: {
+    fontSize: 13,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.heading,
+    lineHeight: 21,
+  }
 })

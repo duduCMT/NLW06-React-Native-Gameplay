c56646cab8689b04b0e80f4e0287c200a5c90615
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2022-01-05 21:58:45 -0300

    Exibindo dados do usuario vindo da api na interface

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -15,7 +15,7 @@ import { api } from '../services/api'
 type User = {
   id: string;
   username: string;
-  fistName: string;
+  firstName: string;
   avatar: string;
   email: string;
   token: string;
@@ -49,14 +49,10 @@ export function AuthProvider({ children }: AuthProviderProps){
 
       const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
       const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse
-
-      
-
       if(type === 'success'){
         api.defaults.headers.common['authorization'] = `Bearer ${params.access_token}`
         
         const userInfo = await api.get('/users/@me')
-
         const firstName = userInfo.data.username.split(' ')[0]
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
 

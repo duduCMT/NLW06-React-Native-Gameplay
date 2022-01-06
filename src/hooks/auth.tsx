2fc0ec8efed98d6c425a5925833d0395d529c51b
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2022-01-06 19:39:39 -0300

    Salvando usuario autenticado localmente com async storage

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,7 +1,7 @@
-import React, { createContext, ReactNode, useContext, useState } from 'react'
+import React, { createContext, ReactNode, useContext, useState, useEffect } from 'react'
 
 import * as AuthSession from 'expo-auth-session'
-
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const { SCOPE } = process.env
 const { CLIENT_ID } = process.env
@@ -9,8 +9,8 @@ const { CDN_IMAGE } = process.env
 const { REDIRECT_URI } = process.env
 const { RESPONSE_TYPE } = process.env
 
-
 import { api } from '../services/api'
+import { COLLECTION_USERS } from '../configs/database'
 
 type User = {
   id: string;
@@ -40,30 +40,33 @@ type AuthorizationResponse = AuthSession.AuthSessionResult & {
   }
 }
 
-export function AuthProvider({ children }: AuthProviderProps){
+export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User)
   const [loading, setLoading] = useState(false)
 
-  async function singIn (){
+  async function singIn() {
     try {
       setLoading(true)
 
       const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
       const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse
 
-      if(type === 'success' && !params.error){
+      if (type === 'success' && !params.error) {
         api.defaults.headers.common['authorization'] = `Bearer ${params.access_token}`
-        
+
         const userInfo = await api.get('/users/@me')
         const firstName = userInfo.data.username.split(' ')[0]
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
 
-        setUser({
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        })
-      } 
+        }
+
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData))
+        setUser(userData)
+      }
     } catch {
       throw new Error('Não foi possível autenticar')
 
@@ -71,6 +74,20 @@ export function AuthProvider({ children }: AuthProviderProps){
       setLoading(false)
     }
   }
+
+  async function loadUserStorageData() {
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS)
+    if(storage){
+      const userLogged = JSON.parse(storage) as User
+      api.defaults.headers.common['authorization'] = `Bearer ${userLogged.token}`
+      
+      setUser(userLogged)
+    }
+  }
+
+  useEffect(() => {
+    loadUserStorageData()
+  }, [])
 
   return (
     <AuthContext.Provider value={{
@@ -83,6 +100,6 @@ export function AuthProvider({ children }: AuthProviderProps){
   )
 }
 
-export function useAuth(){
+export function useAuth() {
   return useContext(AuthContext)
 }
